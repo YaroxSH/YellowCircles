7bd6716779f0ed94456b700fb4a4e7bdf1de5084
bstrk <bstrk@vk.com>
2023-11-20 19:48:58 +0300

    drawing circles

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.paint = False
+
+    def run(self):
+        self.paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor('yellow'))
+        w, h = self.geometry().width(), self.geometry().height()
+        x, y = random.randrange(0, w - 20), random.randrange(0, h - 20)
+        d = random.randrange(10, w - 30)
+        qp.drawEllipse(x, y, d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
